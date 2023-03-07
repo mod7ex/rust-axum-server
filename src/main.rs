c6b3b5755d43112c3df29ddb869ae5f06b5dafaf
Mourad EL CADI <mouradface1998@gmail.com>
2023-03-07 01:11:57 +0100

    added sys info for CPUs state view

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use axum::{Router, Server, routing::get};
-use std::net::SocketAddr;
+use axum::{Router, Server, routing::get, extract::State};
+use std::{net::SocketAddr, sync::{Mutex, Arc}};
+
+use sysinfo::{CpuExt, System, SystemExt};
 
 #[tokio::main]
 async fn main() {
     let router = Router::new()
-        .route("/", get(handler));
+        .route("/", get(handler))
+        .with_state(AppState {
+            sys: Arc::new(Mutex::new(System::new()))
+        });
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
@@ -16,6 +21,25 @@ async fn main() {
     server.await.unwrap();
 }
 
-async fn handler() -> &'static str {
-    "Hi bro"
+#[derive(Clone)]
+struct AppState {
+    sys: Arc<Mutex<System>>
+}
+
+async fn handler(State(state): State<AppState>) -> String {
+    use std::fmt::Write;
+
+    let mut sys = state.sys.lock().unwrap();
+
+    sys.refresh_cpu();
+
+    let mut output = String::new();
+
+    for cpu in sys.cpus() {
+        let n = cpu.name();
+        let v = cpu.cpu_usage().to_string();
+        write!(output, "\n [{n}]: {v}%").unwrap();
+    }
+
+    output
 }
